d49283378210503dc19502dd6e12e8d819ce8b9e
Ömer <lgoh597@gmail.com>
2022-12-27 22:09:39 +0100

    Day 1

---
+++ src/day02_EscapeSequences/AboutMe.java
@@ -0,0 +1,23 @@
+package day02_EscapeSequences;
+
+/*
+1. Create a class named AboutMe.java
+2. Write a code to print the following info about you:
+1. Your favorite book
+2. Your favorite quote
+3. Your favorite TV show
+4. Your favorite music
+ */
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.print("Favorite Book:");
+        System.out.println("\t\t Chess - Stefan Zweig");
+        System.out.print("Favorite Quote:");
+        System.out.println("\t\t All supply generates their own demands");
+        System.out.print("Favorite TV Show:");
+        System.out.println("\t Friends");
+        System.out.print("Favotite Music:");
+        System.out.println("\t\t Ego");
+
+    }
+}
